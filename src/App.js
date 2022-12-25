4fd645020a45d725626ac486f960441d0f5c8b85
Ronny Toribio Blanco <ronnytoribio1@hotmail.com>
2022-12-25 09:37:22 -0500

    removed unessary componets

---
+++ src/App.js
@@ -0,0 +1,127 @@
+import './App.css';
+import { useState, useRef } from 'react';
+import { Line } from 'react-chartjs-2';
+import 'chartjs-plugin-dragdata'
+
+function App() {
+  const [rpm, setRpm] = useState([])
+  const [nm, setNm] = useState([])
+  const [bhp, setBhp] = useState([])
+  const [powerText, setPowerText] = useState('')
+  const ref = useRef([])
+
+  ref.rpm = rpm
+  ref.nm = nm
+  ref.bhp = bhp
+
+  const handleClick = (e) => {
+    let power = document.getElementsByTagName('textarea')[0].value
+    let powerArray = power.split(/\r|\n/g)
+  
+    let preRpm = []
+    let prenm = []
+    let prebhp = []
+
+    for (let i of powerArray) {
+      let rpmValue = i.split('|')[0]
+      let nmValue = i.split('|')[1]
+      let bhpValue = converToBhp(rpmValue, nmValue)
+
+      preRpm.push(rpmValue)
+      prenm.push(nmValue)
+      prebhp.push(bhpValue)
+    }
+
+    setRpm(preRpm)
+    setNm(prenm)
+    setBhp(prebhp)
+    setPowerText(power)
+  }
+
+  const updatedPower = () => {
+    let str = ''
+    for( let i = 0; i < ref.rpm.length; i++) {
+      str = str + `${ref.rpm[i]}|${ref.nm[i]}\n`
+    }
+    console.log(str)
+    setPowerText(str)
+  }
+
+  const handleChange = e => {
+    setPowerText(e.target.value)
+  }
+
+  const converToBhp = (number, torque) => {
+    return (number * torque) / 7127
+  }
+
+  ref.rpm = rpm
+  ref.nm = nm
+  const labels = ref.rpm;
+  
+  const options = {
+    responsive: true,
+    scales: {
+      y:{
+        position: 'right',
+      }
+    },
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+      dragData: {
+        round: 1,
+        onDragStart: (e) => {
+          console.log(e)
+        },
+        onDrag: (e, datasetIndex, index, value) => {
+          // console.log(datasetIndex, index, value)
+        },
+        onDragEnd: (e, datasetIndex, index, value) => {
+          console.log(datasetIndex, ref.rpm[index], value)
+          updatedPower()
+        }
+      },
+    },
+  };
+  
+  const data = {
+    labels,
+    datasets: [{
+      label: 'Nm',
+      data: ref.nm,
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      tension: 0.1,
+      dragData: true
+    },{
+      label: 'BHP',
+      data: ref.bhp,
+      borderColor: 'rgb(12, 87, 248)',
+      backgroundColor: 'rgba(12, 87, 248, 0.5)',
+      tension: 0.1,
+      dragData: false
+    }],
+  };
+  
+  // console.log(rpm)
+  // console.log(nm)
+
+  return(
+    <>
+      <div>
+        <textarea onChange={handleChange} value={powerText}></textarea>
+        <button onClick={handleClick}>Enter</button>
+      </div>
+      
+      <Line options={options} data={data} plugins={options.plugins}/>
+    </>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './container/App';
+import App from './App';
 import {
   Chart as ChartJS,
   CategoryScale,
