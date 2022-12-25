8db6dc0b5c900f4f144176a2d1cf4d670f2f3514
Ronny Toribio Blanco <ronnytoribio1@hotmail.com>
2022-12-25 01:02:41 -0500

    was able to ubpload new data

--- src/component/CarChart.js
+++ src/component/CarChart.js
@@ -1,11 +1,16 @@
 import { Line } from 'react-chartjs-2';
 import 'chartjs-plugin-dragdata'
 
-function CarChart() {
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+function CarChart({rpm, bhp}) {
+  const labels = rpm;
 
   const options = {
     responsive: true,
+    scales: {
+      y:{
+        position: 'right',
+      }
+    },
     plugins: {
       legend: {
         position: 'top',
@@ -23,7 +28,7 @@ function CarChart() {
           // console.log(datasetIndex, index, value)
         },
         onDragEnd: (e, datasetIndex, index, value) => {
-          console.log(datasetIndex, labels[index], value)
+          console.log(datasetIndex, rpm[index], value)
         }
       },
     },
@@ -33,21 +38,24 @@ function CarChart() {
     labels,
     datasets: [{
       label: 'Dataset 1',
-      data: [0, 20, 20, 60, 60, 120, NaN, 180, 120, 125, 105, 110, 170],
+      data: bhp,
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
       tension: 0.1,
       dragData: true
-    },{
-      label: 'Dataset 2',
-      data: [0, 20, 20, 60, 60, 120, NaN, 180, 120, 125, 105, 110, 170],
-      borderColor: 'rgb(12, 87, 248)',
-      backgroundColor: 'rgba(12, 87, 248, 0.5)',
-      tension: 0.1,
-      dragData: true
-    }],
+    }]
+    // },{
+    //   label: 'Dataset 2',
+    //   data: [0, 20, 20, 60, 60, 120, NaN, 180, 120, 125, 105, 110, 170],
+    //   borderColor: 'rgb(12, 87, 248)',
+    //   backgroundColor: 'rgba(12, 87, 248, 0.5)',
+    //   tension: 0.1,
+    //   dragData: true
+    // }],
   };
-
+  
+  console.log(rpm)
+  console.log(bhp)
   
   return <Line options={options} data={data} plugins={options.plugins}/>;
 }
--- src/container/App.js
+++ src/container/App.js
@@ -1,12 +1,42 @@
 import './App.css';
+import { useState } from 'react';
 import CarChart from '../component/CarChart';
 
+
 function App() {
-  return (
-    <div className="App">
-      <CarChart />
-    </div>
-  );
+  const [power, setPower] = useState('')
+  const [rpm, setRpm] = useState([])
+  const [bhp, setBhp] = useState([])
+
+  const handleClick = (e) => {
+    setPower(document.getElementsByTagName('textarea')[0].value)
+    convertToData()
+  }
+
+  const convertToData = () => {
+    let powerArray = power.split(/\r|\n/g)
+    
+    let preRpm = []
+    let preBhp = []
+
+    for (let i of powerArray) {
+      preRpm.push(i.split('|')[0])
+      preBhp.push(i.split('|')[1])  
+    }
+
+    setRpm(preRpm)
+    setBhp(preBhp)
+  }
+
+  return(
+    <>
+      <div>
+        <textarea></textarea>
+        <button onClick={handleClick}>Enter</button>
+      </div>
+      <CarChart rpm={rpm} bhp={bhp}/>
+    </>
+  )
 }
 
 export default App;
